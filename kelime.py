65634967c5dfd37e78667f4dedf1684dd197ddc1
Aydoğan <aydogan.gokberk@gmail.com>
2020-05-20 16:48:37 +0300

    kod açıklamaları eklendi

--- kelime.py
+++ kelime.py
@@ -3,9 +3,11 @@ firstLetter = input("İLK HARF: ")
 lastLetter = input("SON HARF: ")
 
 wordList = open("full.txt","r", encoding="utf-8") # full.txt dosyasını utf-8 e çevirerek açar
-word = wordList.read().splitlines() 
+word = wordList.read().splitlines() # keliemeleri liste haline getirdik 
 
 for i in word:
     w = i.split()[0]
+    # full.txt dosyasında kelimeler yanlarında numaralarla olduğu için .split ile kelimeleri ayırdık.
+    # .split default olarak whitespace ile öğeyi ayırıyor. [0] ile ilk olanı yani kelimeyi seçmiş olduk.
     if w.startswith(firstLetter) and w.endswith(lastLetter):
         print(w)
